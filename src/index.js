68282d649e1dfc62b49e450ee2753f43a2c56b74
Yash Kapoor <yash.kpr2708@gmail.com>
2025-06-29 14:08:49 +0530

    Added validation class

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const apiRoutes = require('./routes/index');
 
+const { User , Role } = require('./models/index')
+
 // const db = require('./models/index');
 
 const { PORT } = require('./config/serverConfig');
@@ -16,11 +18,25 @@ const prepareAndStartServer = () => {
     
     app.use('/api', apiRoutes);
 
-    app.listen(3001, () => {
+    app.listen(3001, async () => {
         console.log(`Server Started at port ${PORT}`);
         // if(process.env.DB_SYNC) {
         //     db.sequelize.sync({alter: true});
         // }
+
+        // manually adding user role
+        // const u1 = await User.findByPk(4);
+        // const r1 = await Role.findByPk(1);
+        // u1.addRole(r1);
+
+        // to print role of user
+        // const response = await u1.getRoles();
+        // console.log(response);
+
+        // to get all users of this Role
+        // const response = await r1.getUsers();
+        // console.log(response);
+
     });
 }
 
